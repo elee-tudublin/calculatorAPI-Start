c3da4fb90111a92718440c07c1c2a42177ad9262
elee-tudublin <55653651+elee-tudublin@users.noreply.github.com>
2021-10-11 11:01:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use('/calculator', require('./controllers/calculator'));
 
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
     var err = new Error('Not Found: '+ req.method + ":" + req.originalUrl);
     err.status = 404;
     next(err);
